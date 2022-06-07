c7f835f3a579fe8cb809beda06465ab66b6e2bce
Emerson <emerson29988@gmail.com>
2022-06-06 22:45:21 -0300

    Adding some texts

--- src/functions/cloudfront/text.ts
+++ src/functions/cloudfront/text.ts
@@ -1,15 +1,29 @@
 const text = {
   titles: ["Para o amor de toda minha vidinha", "Musa da OMS"],
   paragraphes: [
-    "Sério mesmo que tu achou que a pessoa que não tem coordenação motora pra comer com hashi ia fazer algum presente com as mãos e arriscar arruinar o primeiro dia dos namorados??? \
+    'Sério mesmo que tu achou que a pessoa que não tem coordenação motora pra comer com hashi ia fazer algum presente com as mãos e arriscar arruinar o primeiro dia dos namorados??? \
     Inocente demais você, mas quem diria que uma corridinha de manhã ia virar tudo isso, que nosso segundo encontro seria na praia com os seus pais e que em pouco eu estaria \
     esperando você e dona Eralda amolarem alicates num sábado de manhã, foram tantas coisas que aconteceram tão rápido pra quem queria ir devagar kkkj, \
     e mesmo quando você levantou uma bandeirinha vermelha não deu pra trás, até porque quem anda de costas é carangueijo não é? Uma princesinha (muito clichê isso) \
-    que se transformou na Fiona, momentos marcados por vários \"Que sabor\" e alguns \"Rabuuuuuda, Xere....\" que me fazem rir e as vezes me assustam quando vêm de repente. \
+    que se transformou na Fiona, momentos marcados por vários "Que sabor" e alguns "Rabuuuuuda, Xere...." que me fazem rir e as vezes me assustam quando vêm de repente. \
     (In)Felizmente tudo começou no 100% de cara, com certeza não é uma história de amor que podemos contar para crianças e nem tão pouco seria roteiro pra filme de princesa da disney, \
-    mas é foi uma ótima forma de começar, fugindo do comum e totalmente na ocasionalidade",
-    "Porque eu te amo",
-    "Coisas engraçadas"],
+    mas é foi uma ótima forma de começar, fugindo do comum e totalmente na ocasionalidade',
+    '"Como vc diferencia amor de paixão?", essa foi uma das perguntas que você me fez logo na primeira vez que eu disse que te amava, de inicio foi totalmente frustrante, \
+    mas durante a conversa eu percebi que você tinha seus motivos pra duvidar, só que agora, depois desse tempo todo eu sei bem diferenciar amor de paixão, não com palavras, \
+    mas com sentimentos, amor é quando eu estou com você e esqueço qualquer coisa além do que estamos fazendo porque tudo que me importa já está ali comigo, amor é quando eu \
+    estou longe de você e fico triste porque não estou ao seu lado pra ouvir sua voz, sentir seu cheiro e te encher de beijinho, amor é quando eu vou dormir sorrindo por saber \
+    que no dia seguinte a gente vai se ver e teremos um tempo pra nós dois novamente, eu sinceramente não sei distinguir amor de paixão em relação a você, porque por mais que \
+    tenha sido rápido demais, eu te amei desde o inicio, You had me at hello.',
+    'A esse ponto eu acho que já dá pra ser sincero, eu fiz isso tudo porque é bem menos trabalhoso do que tentar consertar a minha caligrafia horrível, tu já viu e eu quero que \
+    esse seja um momento feliz e não um momento do tipo "Que que tá escrito aqui?". Mas enfim, eu tentei ser o mais original possível nessa surpresa e tentei te \
+    confundir ao máximo para que você não fizesse ideia do que isso tudo pudesse ser, espero que tenha funcionando, porque você merece isso e muito mais (menos enfiar o dedo \
+    no meu cu), não sei como seria esse ano se não tivesse você na minha vida, provavelmente seria mais um ano triste para o jovem brasileirinho aqui. Espero muito que esse \
+    o primeiro de muitos anos juntos. Tem uma frase que tá faltando né? Mas calma que vai chegar na hora certa.',
+    "Essa foi a primeira vez que te vi, foi nesse momento que sabia que queria você na minha vida, antes mesmo de te conhecer, você já tinha me deixado bobo. Depois de tudo isso, \
+    ficou faltando te dizer uma coisa, mas foi premetitado pra ser aqui no final pois tudo que falei aqui demonstra um pouquinho desse sentimento, \
+    mas direto ao ponto, Eu te amo Clara, se eu pudesse voltar no passado, eu escolheria te amar todas as vezes, não há nenhuma outra mulher com quem eu gostaria de estar \
+    hoje que não fosse você, te amo nesse e em todos os outros multiversos! ❤️",
+  ],
   cards: [
     "Infelizmente descobri que eu não sou seu primeiro namorado e cheguei tarde demais pra ser o seu primeiro \
     amor, mas espero ser o seu último namorado e o primeiro cara que vai te fazer feliz durante muito tempo, e que também \
@@ -26,9 +40,9 @@ const text = {
     nunca faria por livre e expontanea vontade, não que isso seja algo ruim, mas o tempo que passamos junto com argila na cara tal qual dois \
     interpretes de Shrek e Fiona (e as vezes Grinch), me deixa muito bem. Obrigado por ser uma boa influencia aos meus hábitos, \
     alguns deles pelo menos, dizendo melhor, a menor parte deles.",
-    "Nossa primeira \"Viagem\" juntos, mesmo que não tenha sido pra Europa ou Canadá e sim Simões Filho (kkkj), foi um grande passo que \
-    demos em pouco tempo, isso já trouxe boas histórias pra gente contar no futuro pros sobrinhos, tipo \"A história daquela vez que a gente \
-    quase foi mordido por um cavalo\" ou \"O dia em que eu mostrei minhas incríveis habilidades de motorista pra você\".",
+    'Nossa primeira "Viagem" juntos, mesmo que não tenha sido pra Europa ou Canadá e sim Simões Filho (kkkj), foi um grande passo que \
+    demos em pouco tempo, isso já trouxe boas histórias pra gente contar no futuro pros sobrinhos, tipo "A história daquela vez que a gente \
+    quase foi mordido por um cavalo" ou "O dia em que eu mostrei minhas incríveis habilidades de motorista pra você".',
   ],
 };
 
